57b5fd6111424c9e613d23e759f799f15fe95d52
Frederich <shaoyenc@usc.edu>
2014-04-12 06:35:59 -0700

    Redesign about personal photo frames. Fix css issues.

---
+++ fuel/application/views/_layouts/house_detail_left.php
@@ -0,0 +1,59 @@
+
+<html>
+    <head>
+        <title>USC TSA</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" type="text/css" href="assets/css/housePicDetail.css">
+        <script type="text/javascript" src="assets/js/jquery.js"></script>
+        <script type="text/javascript" src="assets/js/slidepics.js"></script>
+    </head>
+
+
+    <body>
+        
+        <div id="showbox">
+            <div id="showImg">
+                <table>
+                    <tr>
+                        <td align="center" valign="center" bgcolor="#FFFFFF">
+                            <img src="assets/images/b/house_pic1.jpg" id="imgB" />
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            
+            <div id="imgList">    	
+                <span id="prv"><a href="#"><img src="assets/images/left_arrow.gif" border="0"/></a></span>           
+                <div id="list">
+                    <ul>
+                        <li class="on"><img src="assets/images/s/house_pic1.jpg" /></li>
+                        <li><img src="assets/images/s/house_pic2.jpg" /></li>
+                        <li><img src="assets/images/s/house_pic3.jpg" /></li>
+                        <li><img src="assets/images/s/house_pic4.jpg" /></li>
+                        <li><img src="assets/images/s/house_pic5.jpg" /></li>
+                        <li><img src="assets/images/s/house_pic6.jpg" /></li>
+                    
+                    </ul>
+                </div>
+                <span id="next"><a href="#"><img src="assets/images/right_arrow.gif" border="0"/></a></span>    
+            </div>
+        </div>
+        
+        <div id="description">
+            <strong>Description:</strong>This is a cozy apartment locating near USC campus.
+                            There are 1 bedroom and 1 bathroom. 
+                            House owner is very nice...<br />
+                            <br />
+            <strong>Location:</strong>2701 W 23rd St. Los Angeles, CA<br/>
+            <br />
+            <strong>style:</strong>1B1B<br />
+            
+            <h4 id="price">Price: 800/month</h4>
+            <br /> 
+            <iframe src="http://www.dr2ooo.com/tools/maps/maps.php?zoom=15&width=300&height=225&ll=34.029076,-118.281919&ctrl=true&cp=true&" width="300" height="225"></iframe>
+                            
+        </div>
+    </body>
+</html>
+
