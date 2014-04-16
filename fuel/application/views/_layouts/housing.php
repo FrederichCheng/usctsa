01d311022eff47ffc5fbeb5bc5ccd3a58d1b4004
ElvisLee0725 <elvislee0725@gmail.com>
2014-04-16 15:46:50 -0700

    Modify housing php to make it without left and right. Uploading more house pictures.

--- fuel/application/views/_layouts/housing.php
+++ fuel/application/views/_layouts/housing.php
@@ -22,20 +22,22 @@
 
     <div class="section">
         <h2>Long Term Contract</h2>
+        
+        <div id="long_term">
         <?php foreach ($sections as $value) { ?>
-            <table class="housing" border="2" cellpadding="" cellspacing="">
+            <table class="housing" border="2">
                 <tr>
-                    <td width="200" text-align="center">
-                        <img src="assets/images/<?= $value['house_img_left'] ?>" alt="House image" no="1" class="housing_pic img-thumbnail" />
+                    <td >
+                        <img src="assets/images/<?= $value['house_img'] ?>" alt="House image" no="1" class="housing_pic img-thumbnail" />
 
-                        <!-- House Details -->
+                        <!-- House Details show in dialog-->
                         <div id="dialog1" class="dialog" title="House Details:">
                             <div id="showbox">
                                 <div id="showImg">
                                     <table>
                                         <tr>
-                                            <td align="center" valign="center">
-                                                <img src="assets/images/<?= $value['house_img_left'] ?>"  class="imgB img-thumbnail"/>
+                                            <td >
+                                                <img src="assets/images/<?= $value['house_img'] ?>"  class="imgB img-thumbnail"/>
                                             </td>
                                         </tr>
                                     </table>
@@ -44,14 +46,14 @@
                                     <span id="prv"><a href="#"><img src="assets/images/left_arrow.gif" border="0"/></a></span>           
                                     <div id="list">
                                         <ul>
-                                            <li class="on"><img src="assets/images/<?= $value['sImg1_l'] ?>" /></li>
-                                            <li><img src="assets/images/<?= $value['sImg2_l'] ?>" /></li>
-                                            <li><img src="assets/images/<?= $value['sImg3_l'] ?>" /></li>
-                                            <li><img src="assets/images/<?= $value['sImg4_l'] ?>" /></li>
-                                            <li><img src="assets/images/<?= $value['sImg5_l'] ?>" /></li>
-                                            <li><img src="assets/images/<?= $value['sImg6_l'] ?>" /></li>
-                                            <li><img src="assets/images/<?= $value['sImg7_l'] ?>" /></li>
-                                            <li><img src="assets/images/<?= $value['sImg8_l'] ?>" /></li>
+                                            <li class="on"><img src="assets/images/<?= $value['sImg1'] ?>" /></li>
+                                            <li><img src="assets/images/<?= $value['sImg2'] ?>" /></li>
+                                            <li><img src="assets/images/<?= $value['sImg3'] ?>" /></li>
+                                            <li><img src="assets/images/<?= $value['sImg4'] ?>" /></li>
+                                            <li><img src="assets/images/<?= $value['sImg5'] ?>" /></li>
+                                            <li><img src="assets/images/<?= $value['sImg6'] ?>" /></li>
+                                            <li><img src="assets/images/<?= $value['sImg7'] ?>" /></li>
+                                            <li><img src="assets/images/<?= $value['sImg8'] ?>" /></li>
                                           
 
                                         </ul>
@@ -62,15 +64,15 @@
 
 
                             <div class="description">
-                                <strong>Description:</strong><?= $value['description_left'] ?><br />
+                                <strong>Description:</strong><?= $value['description'] ?><br />
                                 
-                                <strong>Location:</strong><?= $value['location_left'] ?><br/>
-                                <strong>Style:</strong><?= $value['style_left'] ?><br />
-                                <strong>Parking:</strong><?= $value['parking_left'] ?><br/>
-                                <strong>Tel:</strong><?= $value['phone_left'] ?><br />
-                                <strong>Email:</strong><?= $value['email_left'] ?><br />
+                                <strong>Location:</strong><?= $value['location'] ?><br/>
+                                <strong>Style:</strong><?= $value['style'] ?><br />
+                                <strong>Parking:</strong><?= $value['parking'] ?><br/>
+                                <strong>Tel:</strong><?= $value['phone'] ?><br />
+                                <strong>Email:</strong><?= $value['email'] ?><br />
                                 <br />
-                                <h4 id="price">Price: <?= $value['price_left'] ?> </h4>
+                                <h4 id="price">Price: <?= $value['price'] ?> </h4>
                                 
                                 <?= $value['house_map'] ?>
                             </div>
@@ -79,79 +81,19 @@
                     </td>
 
                     <td class="housing_descript" valign="top">
-                        <strong>Location:</strong> <?= $value['location_left'] ?> <br /> 
-                        <strong>Price:</strong> <?= $value['price_left'] ?>/month<br/><br />
+                        <strong>Location:</strong> <?= $value['location'] ?> <br /> 
+                        <strong>Price:</strong> <?= $value['price'] ?>/month<br/><br />
 
                         <strong>Description:</strong>
-                        <?= $value['description_left'] ?>
+                        <?= $value['description'] ?>
                         <br /> <br />
 
                     </td>
-                    
-<!-- Right one house -->
-
-                    <td width="200" text-align="center">
-                        <img src="assets/images/<?= $value['house_img_right'] ?>" alt="House image" no="2" class="housing_pic img-thumbnail ">
-                        
-
-                        <!-- House Details -->
-                        <div id="dialog2" class="dialog" title="House Details:">
-                            <div id="showbox">
-                                <div id="showImg">
-                                    <table>
-                                        <tr>
-                                            <td align="center" valign="center">
-                                                <img src="assets/images/b/<?= $value['house_img_right'] ?>" class="imgB img-thumbnail" />
-                                            </td>
-                                        </tr>
-                                    </table>
-                                </div>
-                                <div id="imgList">    	
-                                    <span id="prv"><a href="#"><img src="assets/images/left_arrow.gif" border="0"/></a></span>           
-                                    <div id="list">
-                                        <ul>
-                                            <li class="on"><img src="assets/images/<?= $value['sImg1_r'] ?>" /></li>
-                                            <li><img src="assets/images/<?= $value['sImg2_r'] ?>" /></li>
-                                            <li><img src="assets/images/<?= $value['sImg3_r'] ?>" /></li>
-                                            <li><img src="assets/images/<?= $value['sImg4_r'] ?>" /></li>
-                                            <li><img src="assets/images/<?= $value['sImg5_r'] ?>" /></li>
-                                            <li><img src="assets/images/<?= $value['sImg6_r'] ?>" /></li>
-
-                                        </ul>
-                                    </div>
-                                    <span id="next"><a href="#"><img src="assets/images/right_arrow.gif" border="0"/></a></span>    
-                                </div>
-                            </div>
-
-
-                            <div id="description">
-                                <strong>Description:</strong><?= $value['description_right'] ?><br />
-                                <br />
-                                <strong>Location:</strong><?= $value['location_right'] ?><br/>
-                                <br />
-                                <strong>Style:</strong><?= $value['style_right'] ?><br />
-                                <br />
-                                <h4 id="price">Price: <?= $value['price_right'] ?> </h4>
-                                <br /> 
-                                <iframe src="http://www.dr2ooo.com/tools/maps/maps.php?zoom=15&width=300&height=225&ll=34.029076,-118.281919&ctrl=true&cp=true&" width="300" height="225"></iframe>
-                            </div>
-
-                        </div>
-                    </td>
-                    <td class="housing_descript" valign="top">
-                        <strong>Location:</strong> <?= $value['location_right'] ?> <br /> 
-                        <strong>Price:</strong> <?= $value['price_right'] ?>/month<br/><br />
-
-                        <strong>Description:</strong> 
-                        <?= $value['description_right'] ?>
-                        <br /> <br />
-
-                    </td>
-
-            </table>
-            <br /> <br />
-        <?php } ?>    
-
+                   </table>
+           
+        <?php } ?>   
+            
+            </div>
 
 
         <br/><br/>
