ecd30102bb2bd9ee34ca37cc175e01f085d355b2
ElvisLee0725 <elvislee0725@gmail.com>
2014-04-21 16:51:35 -0700

    modify housing.php and make no sublease section.

--- fuel/application/views/_layouts/housing.php
+++ fuel/application/views/_layouts/housing.php
@@ -7,12 +7,12 @@
 
 <div id="wrapper">   <!--nav scrollbar on the left-->
 
-    <div id="navscroll" class="PageScrollerNav standardNav right dark">
+<!--    <div id="navscroll" class="PageScrollerNav standardNav right dark">
         <ul>
             <li><A href="#">Long Term Contract</A></li>
             <li><A href="#">Sublease</A></li>
         </ul>
-    </div>
+    </div>-->
 
     <div>
         <h1>Housing</h1>
@@ -98,42 +98,7 @@
 
 
         <br/><br/>
-        <!-- For sublease -->
-        <div class="section">
-            <h2>Sublease</h2>
-
-            <table class="housing" border="2" cellpadding="" cellspacing="">
-                <tr>
-                    <td class="housing_pic" align="left" valign="top">
-                        <A href="images/house_pic3.jpg" target="_blank"><img src="images/house_pic3.jpg" alt="House image" width="160" height="150">
-                        </A>   
-                    </td>
-                    <td class="housing_descript" valign="top">
-                        <br />
-                        Description:
-                        This is a cozy apartment locating near USC campus.
-                        There are 1 bedroom and 1 bathroom. 
-                        House owner is very nice...<br /> <br />
-                        Location: 2423 W 23rd St.<br /> <br />
-                        Price: 800/month<br/>
-
-                    </td>
-                    <td class="housing_pic" align="left" valign="top">
-                        <A href="images/house_pic4.jpg" target="_blank"><img src="images/house_pic4.jpg" alt="House image" width="160" height="150">
-                        </A> 
-                    </td>
-                    <td class="housing_descript" valign="top">
-                        <br />
-                        This is a cozy apartment about 10mins walking distance to USC campus.
-                        There are 1 bedroom and 1 bathroom. 
-                        House owner is very nice...<br /><br />
-                        Location: 2423 W 23rd St.<br /> <br />
-                        Price: 750/month<br/>
-                    </td>
-                </tr>
-            </table>
-        </div>
-        <!--      </div>  -->
+       
 
         <script type="text/javascript">
             $(document).ready(function() {
