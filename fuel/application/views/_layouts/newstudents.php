90489633ffa1098816cf942e9691224cffe9f3e1
ElvisLee0725 <elvislee0725@gmail.com>
2014-06-14 13:11:42 -0700

    Modified the about -> members to present 3 members in one row.

--- fuel/application/views/_layouts/newstudents.php
+++ fuel/application/views/_layouts/newstudents.php
@@ -102,15 +102,15 @@
 <div class="section">
         <h1><?= lang('tsa_housing_apartment') ?></h1>
         <div class="housing-map" style="height:500px;">
-            
-            
+              <?php $link="https://mapsengine.google.com/map/u/0/kml?mid=zYjzcxdR19fg.kvE0EHfKh8tc&cid=mp&cv=yCsoPjplrX0.zh_TW" ?>
             
         </div>
         
         <script type="text/javascript">
             (function(){
                 var canva = $('.housing-map').get(0);
-                var url = 'https://mapsengine.google.com/map/u/0/kml?mid=zYjzcxdR19fg.kvE0EHfKh8tc&cid=mp&cv=yCsoPjplrX0.zh_TW';
+                var url = <?php $link ?>;
+                                                                
                 var usc = new google.maps.LatLng(34.02541587908883,-118.27750042546387);
                 
                 var mapOptions = {
