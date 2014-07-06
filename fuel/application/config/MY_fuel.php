00ed8de9acbbdec76c229e193166c3416f2075c0
Frederich <shaoyenc@usc.edu>
2014-07-06 15:17:49 -0700

    Add justify ckeditor plugin

--- fuel/application/config/MY_fuel.php
+++ fuel/application/config/MY_fuel.php
@@ -84,7 +84,7 @@ $config['ck_editor_settings'] = "{
 	bodyClass: 'ckeditor',
 	protectedSource: [/\{fuel_\w+\(.+\)\}/g, /<\?[\s\S]*?\?>/g],
 	toolbarCanCollapse: false,
-	extraPlugins: 'fuellink,fuelimage,font',
+	extraPlugins: 'fuellink,fuelimage,font,justify',
 	removePlugins: 'link,image'
 	}";
 
